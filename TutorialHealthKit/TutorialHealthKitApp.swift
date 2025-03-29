ca75afb9c12ba4e7d47bf5147d3b776753a6a609
Ellen Schrader <mail@ellen-schrader.de>
2025-03-29 20:24:13 +0000

    Initial Commit

---
+++ TutorialHealthKit/TutorialHealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialHealthKitApp.swift
+//  TutorialHealthKit
+//
+//  Created by Ellen Schrader on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TutorialHealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TutorialHealthKitTests/TutorialHealthKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialHealthKitTests.swift
+//  TutorialHealthKitTests
+//
+//  Created by Ellen Schrader on 29/03/2025.
+//
+
+import Testing
+@testable import TutorialHealthKit
+
+struct TutorialHealthKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
